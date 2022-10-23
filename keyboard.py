bad8a01bf3a4ae8c7427a3cb41d6e0fd04ab2435
t0rchik <84737528+t0rch1k@users.noreply.github.com>
2022-10-23 15:28:14 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -4,28 +4,28 @@ from aiogram import types
 
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
 menu.add(
-    types.KeyboardButton('👑 Админка')
+    types.KeyboardButton('👑 admin')
 )
 
 adm = types.ReplyKeyboardMarkup(resize_keyboard=True)
 adm.add(
-    types.KeyboardButton('👿 ЧС'),
-    types.KeyboardButton('✅ Добавить в ЧС'),
-    types.KeyboardButton('❎ Убрать из ЧС')
+    types.KeyboardButton('👿 black list'),
+    types.KeyboardButton('✅ add to blacklist'),
+    types.KeyboardButton('❎ remove from black list')
 )
-adm.add(types.KeyboardButton('💬 Рассылка'))
-adm.add('⏪ Назад')
+adm.add(types.KeyboardButton('💬 Newsletter'))
+adm.add('⏪ Back')
 
 back = types.ReplyKeyboardMarkup(resize_keyboard=True)
 back.add(
-    types.KeyboardButton('⏪ Отмена')
+    types.KeyboardButton('⏪ Cancel')
 )
 
 
 def fun(user_id):
     quest = types.InlineKeyboardMarkup(row_width=3)
     quest.add(
-        types.InlineKeyboardButton(text='💬 Ответить', callback_data=f'{user_id}-ans'),
-        types.InlineKeyboardButton(text='❎ Удалить', callback_data='ignor')
+        types.InlineKeyboardButton(text='💬 Reply', callback_data=f'{user_id}-ans'),
+        types.InlineKeyboardButton(text='❎ Delete', callback_data='ignor')
     )
     return quest
